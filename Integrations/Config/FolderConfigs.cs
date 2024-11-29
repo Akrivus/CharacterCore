1eebd726bfce879bbe215eaebe1143098c354e74
Owen Adrian <akrivus@gmail.com>
2024-11-29 15:38:11 -0700

    Initial commit.

---
+++ Integrations/Config/FolderConfigs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public class FolderConfigs : IConfig
+{
+    public string Type => "folder";
+    public List<string> Prompts { get; set; }
+    public string ReplayDirectory { get; set; }
+    public int ReplayRate { get; set; }
+    public int ReplaysPerBatch { get; set; }
+    public int MaxReplayAgeInMinutes { get; set; }
+    public bool AutoPlay { get; set; }
+    public bool AutoPlayOnEmpty { get; internal set; }
+}
