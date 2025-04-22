6fa3a7d1cd3583abb01503bf5a6be76435818e60
Owen Adrian <akrivus@gmail.com>
2025-04-22 15:26:45 -0700

    Remote control update.

--- RemoteControl.cs
+++ RemoteControl.cs
@@ -1,75 +1,103 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
 public class RemoteControl : MonoBehaviour
 {
-    private Dictionary<string, Action> buttonActions;
+    private Dictionary<string, Action> buttonActions = new Dictionary<string, Action>();
 
-    private void OnEnable()
+    private string pageUpPath;
+    private string pageDownPath;
+
+    public void Configure(RemoteControlConfigs c)
     {
         buttonActions = new Dictionary<string, Action>
         {
-            { "leftButton", Pause },
-            { "leftArrow", Skip },
-            { "rightArrow", Replay },
-            { "upArrow", SpeedUp },
-            { "downArrow", SlowDown },
-            { "pageUp", SportsVolumeUp },
-            { "pageDown", SportsVolumeDown },
-            { "rightButton", Drop },
-            { "contextMenu", ToggleGame }
+            { "leftButton", Select },
+            { "leftArrow", LeftArrow },
+            { "rightArrow", RightArrow },
+            { "upArrow", UpArrow },
+            { "downArrow", DownArrow },
+            { "pageUp", PageUp },
+            { "pageDown", PageDown },
+            { "rightButton", BackButton },
+            { "contextMenu", MenuButton }
         };
+
+        pageUpPath = c.PageUpPath;
+        pageDownPath = c.PageDownPath;
+        InputSystem.onAnyButtonPress.Call(OnPress);
     }
 
     private void Awake()
     {
-        InputSystem.onAnyButtonPress.Call((button) => buttonActions.GetValueOrDefault(button.name)?.Invoke());
+        ConfigManager.Instance.RegisterConfig(typeof(RemoteControlConfigs), "remote_control", (config) => Configure((RemoteControlConfigs)config));
     }
 
-    private void Pause()
+    private void OnPress(InputControl control)
+    {
+        if (buttonActions.TryGetValue(control.name, out var actionToInvoke))
+            actionToInvoke.Invoke();
+    }
+
+    private void Launch(string path)
+    {
+        if (!File.Exists(path))
+            return;
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = path,
+            WorkingDirectory = Path.GetDirectoryName(path),
+            UseShellExecute = true
+        });
+        Application.Quit();
+    }
+
+    private void Select()
     {
         ChatManager.IsPaused = !ChatManager.IsPaused;
     }
 
-    private void Skip()
-    {
-        ChatManager.SkipToEnd = true;
-    }
-
-    private void Replay()
+    private void LeftArrow()
     {
 
     }
 
-    private void SpeedUp()
-    {
-        ActorController.GlobalSpeakingRate -= 0.1f;
-    }
-
-    private void SlowDown()
-    {
-        ActorController.GlobalSpeakingRate += 0.1f;
-    }
-
-    private void SportsVolumeUp()
+    private void RightArrow()
     {
 
     }
 
-    private void SportsVolumeDown()
+    private void UpArrow()
     {
 
     }
 
-    private void Drop()
+    private void DownArrow()
     {
 
     }
 
-    private void ToggleGame()
+    private void PageUp()
+    {
+        Launch(pageUpPath);
+    }
+
+    private void PageDown()
+    {
+        Launch(pageDownPath);
+    }
+
+    private void MenuButton()
+    {
+        Application.Quit();
+    }
+
+    private void BackButton()
     {
 
     }
